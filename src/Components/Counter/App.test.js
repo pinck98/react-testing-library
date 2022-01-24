aeb56cb25f8127dacffc57c9b486c0425ef103bd
pinck98 <bgff454@gmail.com>
2022-01-24 20:39:12 +0200

    second react test

--- src/Components/Counter/App.test.js
+++ src/Components/Counter/App.test.js
@@ -1,12 +1,99 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+/* eslint-disable testing-library/render-result-naming-convention */
 import Recat from 'react'
 import Index from './Index'
-import {render} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 test("if Counter and test text is rendered",() => {
-    // eslint-disable-next-line testing-library/render-result-naming-convention
     const component = render(<Index/>)
-    // eslint-disable-next-line testing-library/prefer-screen-queries
     const header = component.getByTestId('header')
     expect(header.textContent).toBe('Counter and test')
+})
+
+test("counter start with 0 ?", () => {
+    const component = render(<Index/>)
+    const header = component.getByTestId('counter')
+    expect(header.textContent).toBe('0')
+})
+
+test("increase button has the right text", () => {
+    const component = render(<Index/>)
+    const header = component.getByTestId('increase-button')
+    expect(header.textContent).toBe('increase')
+})
+
+test("decrease button has the right text", () => {
+    const component = render(<Index/>)
+    const header = component.getByTestId('decrease-button')
+    expect(header.textContent).toBe('decrease')
+})
+
+test('change input value',() => {
+    const component = render(<Index/>)
+    const input = component.getByTestId('input')
+    fireEvent.change(input,{
+        target : {
+            value : "7"
+        }
+    })
+    expect(input.value).toBe('7')
+})
+
+test('increase counter with increase button',() => {
+    const component = render(<Index/>)
+    const button = component.getByTestId('increase-button')
+    const counter = component.getByTestId('counter')
+    fireEvent.click(button)
+    expect(counter.textContent).toBe('1')
+})
+
+test('decrease counter with decrease button',() => {
+    const component = render(<Index/>)
+    const button = component.getByTestId('decrease-button')
+    const counter = component.getByTestId('counter')
+    fireEvent.click(button)
+    expect(counter.textContent).toBe('-1')
+})
+
+test('input = 5 and click increase button',() => {
+    const component = render(<Index/>)
+    const input = component.getByTestId('input')
+    const counter = component.getByTestId('counter')
+    const button = component.getByTestId('increase-button')
+    fireEvent.change(input,{
+        target : {
+            value : "5"
+        }
+    })
+    fireEvent.click(button)
+    expect(counter.textContent).toBe('5')
+})
+
+test('counter green color',() => {
+    const component = render(<Index/>)
+    const input = component.getByTestId('input')
+    const counter = component.getByTestId('counter')
+    const button = component.getByTestId('increase-button')
+    fireEvent.change(input,{
+        target : {
+            value : "100"
+        }
+    })
+    fireEvent.click(button)
+    expect(counter.style.color).toBe('green')
+})
+
+test('counter red color',() => {
+    const component = render(<Index/>)
+    const input = component.getByTestId('input')
+    const counter = component.getByTestId('counter')
+    const button = component.getByTestId('increase-button')
+    fireEvent.change(input,{
+        target : {
+            value : "-100"
+        }
+    })
+    fireEvent.click(button)
+    expect(counter.style.color).toBe('red')
 })
--- src/Components/Counter/Index.js
+++ src/Components/Counter/Index.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Index() {
+  const [counter,setCounter] = useState(0)
+  const [inputVal,setInputVal] = useState(1)
+
+  const increase = () => {
+    setCounter(counter + inputVal)
+  }
+  const decrease = () => {
+    setCounter(counter - inputVal)
+  }
+  const setInput = (e) => {
+    setInputVal(parseInt(e.target.value))
+  }
+
   return (
     <>
       <h1 data-testid="header">Counter and test</h1>
-      <h3>0</h3>
-      <button>increase</button>
-      <input type="number"></input>
-      <button>decrease</button>
+      <h3 data-testid="counter" style={{color : counter >= 100 ? 'green' : counter <= -100 ? 'red' : 'black'}}>{counter}</h3>
+      <button data-testid="increase-button" onClick={increase}>increase</button>
+      <input type="number" data-testid="input" value={inputVal} onChange={setInput}></input>
+      <button data-testid="decrease-button" onClick={decrease}>decrease</button>
     </>
   );
 }
